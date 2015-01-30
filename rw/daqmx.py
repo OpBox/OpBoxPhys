579fd6f8266c7fb780e2b23e212e24ef8c85f8b2
ekimchi <DeliriumLaboratory@gmail.com>
2015-01-30 09:00:12 -0500

    Synchronized Analog & Digital, Master & Slave Devices

--- rw/daqmx.py
+++ rw/daqmx.py
@@ -1,13 +1,11 @@
 # On Dell Optiplex Physiology computer as of 1/29/2015:
-# Dev1=PCIe-6323: AI 0-7, 16-23. DI: Port0/0:31
-# Dev2=PCI-6225: AI 0-7, 16-23, 32-39, 48-55, 64-71. DI: Port0/0:7
-
-from __future__ import division  # py2
+# Dev1=PCIe-6323: AI: 0-7, 16-23. DI: Port0/0:31
+# Dev2=PCI-6225: AI: 0-7, 16-23, 32-39, 48-55, 64-71. DI: Port0/0:7
 
 from numpy import zeros
 
 # package to control NI
-from ctypes import byref  # also in PyDAQmx
+from ctypes import byref, c_char_p, c_float, create_string_buffer
 from PyDAQmx.DAQmxConstants import (DAQmx_Val_Diff,
                                     DAQmx_Val_Volts,
                                     DAQmx_Val_Rising,
@@ -17,72 +15,119 @@ from PyDAQmx.DAQmxConstants import (DAQmx_Val_Diff,
                                     DAQmx_Val_Auto,
                                     DAQmx_Val_ChanPerLine,
                                     )
-from PyDAQmx.DAQmxTypes import int32
+from PyDAQmx.DAQmxTypes import *
 from PyDAQmx.Task import Task
 
-from .edf import ExportEdf
+# from .edf import ExportEdf
 
 
 class DAQmxReader(Task):
     """Class to interact with NI devices.
+	"""
+    def __init__(self):
+        super().__init__()
 
-    Parameters
-    ----------
-    args : argparse.Namespace
-        arguments specified by the user
-    funct : function
-        function to be called when the recording is being read.
-    """
-    def __init__(self, args, funct):
-        super(DAQmxReader, self).__init__()  # py2
-
-        # this line is also in the EDF input
-        physicalChannel = (args.dev + '/ai' + args.analoginput).encode('utf-8')
         nameToAssignToChannel  = ''.encode('utf-8')  # use default names
-        s_freq = float(args.s_freq)
-        self.buffer_size = int(s_freq * args.buffer_size)
-        self.timeout = args.timeout
-        self.n_chan = args.n_chan
-        self.funct = funct
+        s_freq = 1000.
+        self.buffer_size = int(s_freq * 0.1)
+        self.timeout = 10.
+        minval = -1.
+        maxval = 1.
 
-        # self.CreateAIVoltageChan(physicalChannel, nameToAssignToChannel,
-                                 # DAQmx_Val_Diff, args.minval, args.maxval,
-                                 # DAQmx_Val_Volts, None)
+        # Master Analog task is the parent task. Digital/Slave tasks are children
+        self.MasterDTask = Task()
+        self.SlaveATask = Task()
+        self.SlaveDTask = Task()
+
+        # Master Analog Inputs
         self.CreateAIVoltageChan(b'Dev1/ai0:7, Dev1/ai16:23', nameToAssignToChannel,
-                                 DAQmx_Val_Diff, args.minval, args.maxval,
+                                 DAQmx_Val_Diff, minval, maxval,
                                  DAQmx_Val_Volts, None)
-        # self.CreateAIVoltageChan(b'Dev2/ai0:7, Dev2/ai16:23, Dev2/ai32:39, Dev2/ai48:55, Dev2/ai64:71', nameToAssignToChannel,
-                                 # DAQmx_Val_Diff, args.minval, args.maxval,
-                                 # DAQmx_Val_Volts, None)
-        self.CreateDIChan(b'Dev1/port0/line0:31', nameToAssignToChannel, DAQmx_Val_ChanPerLine) 
-        # self.CreateDIChan(b'Dev2/port0/line0:7', nameToAssignToChannel, DAQmx_Val_ChanPerLine)
+        # Master Digital Inputs
+        self.MasterDTask.CreateDIChan(b'Dev1/port0/line0:31', nameToAssignToChannel, DAQmx_Val_ChanPerLine) 
+
+
+        # Slave Analog Inputs
+        self.SlaveATask.CreateAIVoltageChan(b'Dev2/ai0:7, Dev2/ai16:23, Dev2/ai32:39, Dev2/ai48:55, Dev2/ai64:71', nameToAssignToChannel,
+                                            DAQmx_Val_Diff, minval, maxval,
+                                            DAQmx_Val_Volts, None)
+        # Slave Digital Inputs
+        self.SlaveDTask.CreateDIChan(b'Dev2/port0/line0:7', nameToAssignToChannel, DAQmx_Val_ChanPerLine)
+
+        # Set Master analog sample clock
         self.CfgSampClkTiming(b'', s_freq, DAQmx_Val_Rising,
                               DAQmx_Val_ContSamps, self.buffer_size)
+		# Synchronize Master Digital clock to Master Analog sample clock
+        master_analog_sample_clock = b'/Dev1/ai/SampleClock'
+        self.MasterDTask.CfgSampClkTiming(master_analog_sample_clock, s_freq, DAQmx_Val_Rising,
+                                          DAQmx_Val_ContSamps, self.buffer_size)
+		# Digital devices need to be started first, and slave before master for analog
+        self.MasterDTask.StartTask()
+										  
+		# Set Slave analog sample clock
+        self.SlaveATask.CfgSampClkTiming(b'', s_freq, DAQmx_Val_Rising,
+                                         DAQmx_Val_ContSamps, self.buffer_size)
+                                         
+		# Synchronize Slave Digital clock to Slave Analog sample clock
+        slave_analog_sample_clock = b'/Dev2/ai/SampleClock'
+        self.SlaveDTask.CfgSampClkTiming(slave_analog_sample_clock, s_freq, DAQmx_Val_Rising,
+                                          DAQmx_Val_ContSamps, self.buffer_size)
+        
+		# Route Master analog clock to Slave devices
+        self.SlaveATask.SetRefClkSrc(master_analog_sample_clock)
+        # Get/Set reference clock rates: Master -> Slave devices
+        clkRate = float64()
+        self.GetRefClkRate(byref(clkRate))
+        self.SlaveATask.SetRefClkRate(clkRate)                              
+                              
         self.AutoRegisterEveryNSamplesEvent(DAQmx_Val_Acquired_Into_Buffer,
                                             self.buffer_size, 0)
         self.AutoRegisterDoneEvent(0)
+        
+		# Digital devices need to be started first, and slave before master for analog
+        self.SlaveDTask.StartTask()
+        self.SlaveATask.StartTask()
 
-        self.edf = None
-        if args.edf is not None:
-            self.edf = ExportEdf()
-            self.edf.open(args)
-
+        
     def EveryNCallback(self):
         """Read the recording once buffer on the device is ready.
         """
         read = int32()
-        data = zeros(self.buffer_size * self.n_chan)
+        master_a_nchan = 16
+        slave_a_nchan = 40
+        master_d_nchan = 32
+        slave_d_nchan = 8               
+        
+        # Data arrays for analog data must be Float 64, which is apparently the python default
+        master_adata = zeros(self.buffer_size * master_a_nchan)
         self.ReadAnalogF64(DAQmx_Val_Auto, self.timeout,
-                           DAQmx_Val_GroupByChannel, data,
-                           self.buffer_size * self.n_chan, byref(read), None)
-        self.ReadDigitalU32(DAQmx_Val_Auto, self.timeout,
-                            DAQmx_Val_GroupByChannel, data,
-                            self.buffer_size * self.n_chan, byref(read), None)
+                           DAQmx_Val_GroupByChannel, master_adata,
+                           self.buffer_size * master_a_nchan, byref(read), None)
+        
+        slave_adata = zeros(self.buffer_size * slave_a_nchan)
+        self.SlaveATask.ReadAnalogF64(DAQmx_Val_Auto, self.timeout,
+                                      DAQmx_Val_GroupByChannel, slave_adata,
+                                      self.buffer_size * slave_a_nchan, byref(read), None)
+                                      
+        # Data arrays for Digital data must be unisigned int32
+        master_ddata = zeros(self.buffer_size * master_d_nchan, dtype=uInt32)
+        slave_ddata = zeros(self.buffer_size * slave_d_nchan, dtype=uInt32)
 
-        data = data.reshape((self.n_chan, self.buffer_size))
-        if self.edf is not None:
-            self.edf.write(data)
-        self.funct(data, self.buffer_size)
+        self.MasterDTask.ReadDigitalU32(DAQmx_Val_Auto, self.timeout,
+                            DAQmx_Val_GroupByChannel, master_ddata,
+                            self.buffer_size * master_d_nchan, byref(read), None)
+        self.SlaveDTask.ReadDigitalU32(DAQmx_Val_Auto, self.timeout,
+                            DAQmx_Val_GroupByChannel, slave_ddata,
+                            self.buffer_size * slave_d_nchan, byref(read), None)
+
+                            
+        master_adata = master_adata.reshape((master_a_nchan, self.buffer_size))
+        slave_adata = slave_adata.reshape((slave_a_nchan, self.buffer_size))
+        master_ddata = master_ddata.reshape((master_d_nchan, self.buffer_size))
+        slave_ddata = slave_ddata.reshape((slave_d_nchan, self.buffer_size))
+        
+        print('MA: ' + str(master_adata[0, 0]) + ' SA: ' + str(slave_adata[0, 0]))
+        print('MD: ' + str(master_ddata[0, 0]) + ' SD: ' + str(slave_ddata[0, 0]))
         return 0 # The function should return an integer
 
     def DoneCallback(self, status):
@@ -90,6 +135,18 @@ class DAQmxReader(Task):
 
         Probably raise error if recordings are interrupted
         """
-        if self.edf is not None:
-            self.edf.close()
+        self.SlaveDTask.StopTask()
+        self.SlaveDTask.ClearTask()
+        self.SlaveATask.StopTask()
+        self.SlaveATask.ClearTask()
+        self.MasterDTask.StopTask()
+        self.MasterDTask.ClearTask()
         return 0
+
+		
+reader = DAQmxReader()
+reader.StartTask()
+input('Acquiring samples continuously. Press Enter to interrupt\n')
+reader.StopTask()
+reader.ClearTask()
+		
